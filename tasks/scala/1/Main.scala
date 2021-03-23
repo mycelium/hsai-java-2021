86a08f01760b5567fdeaf05bdd06a55cb8a0f254
Vovan-S <57155164+Vovan-S@users.noreply.github.com>
2021-03-23 16:00:40 +0300

    finished task 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,34 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (r < 0) 0
+    else if (r == 0) {
+      if (c == 0) 1 else 0
+    }
+    else {
+      if (c < 0) 0 else pascal(c, r - 1) +
+        pascal(c - 1, r - 1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def recursion(chars: List[Char], counter: Int): Boolean = {
+      chars match {
+        case Nil => counter == 0
+        case '(' :: (rest: List[Char]) => recursion(rest, counter + 1)
+        case ')' :: (rest: List[Char]) =>
+          if (counter > 0)
+            recursion(rest, counter - 1)
+          else
+            false
+        case _ :: (rest: List[Char]) => recursion(rest, counter)
+        case _ => false
+      }
+    }
+    recursion(chars, 0)
   }
 
   /**
@@ -33,6 +53,28 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    def recursion(money: Int, coins: List[Int]): Int = {
+      if (money == 0) 1
+      else if (money < 0) 0
+      else coins match {
+        case Nil => 0
+        case x :: Nil => if (money % x == 0) 1 else 0
+        case x :: rest => recursion(money - x, coins) +
+          recursion(money, rest)
+      }
+    }
+    if (money <= 0) 0
+    else recursion(money,
+      coins.distinct                    // (1)
+        .filter(_ > 0)                  // (2)
+        .sorted(Ordering[Int].reverse)) // (3)
 
+    // (1) выкидывать повторяющиеся элементы может и не нужно,
+    //     зависит от спецификации
+    // (2) при наличии отрицательных элементов алгоритм не
+    //     завершится, я решил их выкинуть, т.к. спецификации
+    //     нет
+    // (3) сортировка по убыванию заметно уменьшает количество
+    //     вызывов
   }
 }
