130fc5e8122327e7c7278ff4c328207c0b2cca20
Mikhail Malyarenko <malyarenko.md@gmail.com>
2021-05-29 19:16:37 +0300

    Scala Task 3 Done

--- tasks/scala/3/anagrams/src/main/scala/forcomp/Anagrams.scala
+++ tasks/scala/3/anagrams/src/main/scala/forcomp/Anagrams.scala
@@ -1,6 +1,6 @@
 package forcomp
 
-import common._
+import sun.jvm.hotspot.memory.Dictionary
 
 object Anagrams {
 
@@ -33,10 +33,23 @@ object Anagrams {
    *  Note: the uppercase and lowercase version of the character are treated as the
    *  same character, and are represented as a lowercase character in the occurrence list.
    */
-  def wordOccurrences(w: Word): Occurrences = ???
+  //noinspection RemoveRedundantReturn
+  def wordOccurrences(w: Word): Occurrences = {
+    var occurrences : Occurrences = Nil
+    val word : Word = w.toLowerCase
+    val distinctLetters : List[Char] = word.toList.distinct
+
+    for (c : Char <- distinctLetters) {
+      val charCount = word.count(_ == c)
+      val newOccurrence : (Char, Int) = (c, charCount)
+      occurrences = newOccurrence :: occurrences
+    }
+
+    return occurrences.sortWith((x, y) => x._1 < y._1)
+  }
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = wordOccurrences(s.mkString)
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
@@ -53,10 +66,36 @@ object Anagrams {
    *    List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
    *
    */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
+  def fillDictionary(dictionary: List[Word]) : Map[Occurrences, List[Word]] = {
+    var dictionaryMap = Map.empty[Occurrences, List[Word]]
+
+    for (w <- dictionary) {
+      val occurrences : Occurrences = wordOccurrences(w)
+      val mapEntry = dictionaryMap.get(occurrences)
+
+      dictionaryMap = if (mapEntry.isEmpty) {
+        dictionaryMap + (occurrences -> List(w))
+      }
+      else {
+        dictionaryMap + (occurrences -> (w :: mapEntry.get))
+      }
+    }
+
+    return dictionaryMap
+  }
+
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = fillDictionary(dictionary)
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = ???
+  def wordAnagrams(word: Word): List[Word] = {
+    val anagrams = dictionaryByOccurrences.get(wordOccurrences(word))
+    if (anagrams.isEmpty) {
+      return Nil
+    }
+    else {
+      return anagrams.get
+    }
+  }
 
   /** Returns the list of all subsets of the occurrence list.
    *  This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
@@ -80,7 +119,28 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combRecursion(occurrences : Occurrences) : List[Occurrences] = {
+    var currentOccurrences : List[Occurrences] = Nil
+
+    for (o <- occurrences) {
+      val modifiedOccurrences = if (o._2 == 1) {
+        occurrences.filterNot(x => x._1 == o._1)
+      }
+      else {
+        occurrences.map(x => if (x._1 == o._1) (x._1, x._2 - 1) else x)
+      }
+
+      if (!modifiedOccurrences.isEmpty) {
+        currentOccurrences = modifiedOccurrences :: currentOccurrences ::: combRecursion(modifiedOccurrences)
+      }
+    }
+
+    return currentOccurrences
+  }
+
+  def combinations(occurrences: Occurrences): List[Occurrences] = {
+    Nil :: occurrences :: combRecursion(occurrences).distinct
+  }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    * 
@@ -92,7 +152,25 @@ object Anagrams {
    *  Note: the resulting value is an occurrence - meaning it is sorted
    *  and has no zero-entries.
    */
-  def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
+  def subtract(x: Occurrences, y: Occurrences): Occurrences = {
+    if (y.isEmpty) {
+      return x
+    }
+    else {
+      var subOccurrences : Occurrences = Nil
+
+      for (ocX <- x) {
+        val twinOccurrence = y.find(ocY => ocY._1 == ocX._1)
+        if (twinOccurrence.isEmpty) {
+          subOccurrences = ocX :: subOccurrences
+        }
+        else if (ocX._2 > twinOccurrence.get._2){
+          subOccurrences = (ocX._1, ocX._2 - twinOccurrence.get._2) :: subOccurrences
+        }
+      }
+      return subOccurrences.sortWith((x, y) => x._1 < y._1)
+    }
+  }
 
   /** Returns a list of all anagram sentences of the given sentence.
    *  
@@ -110,20 +188,20 @@ object Anagrams {
    *  Here is a full example of a sentence `List("Yes", "man")` and its anagrams for our dictionary:
    *
    *    List(
-   *      List(en, as, my),
-   *      List(en, my, as),
-   *      List(man, yes),
-   *      List(men, say),
-   *      List(as, en, my),
-   *      List(as, my, en),
+   *      List(en, as, my), List(en, as, my)
+   *      List(en, my, as), List(en, my, as)
+   *      List(man, yes), List(man, yes)
+   *      List(men, say), List(men, say)
+   *      List(as, en, my), List(as, en, my)
+   *      List(as, my, en), List(as, my, en)
    *      List(sane, my),
-   *      List(Sean, my),
-   *      List(my, en, as),
-   *      List(my, as, en),
+   *      List(Sean, my), List(Sean, my)
+   *      List(my, en, as), List(my, en, as)
+   *      List(my, as, en), List(my, as, en)
    *      List(my, sane),
-   *      List(my, Sean),
-   *      List(say, men),
-   *      List(yes, man)
+   *      List(my, Sean), List(my, Sean)
+   *      List(say, men), List(say, men)
+   *      List(yes, man) List(yes, man)
    *    )
    *
    *  The different sentences do not have to be output in the order shown above - any order is fine as long as
@@ -134,6 +212,53 @@ object Anagrams {
    *
    *  Note: There is only one anagram of an empty sentence.
    */
-  def sentenceAnagrams(sentence: Sentence): List[Sentence] = ???
+  def sentenceFromOccurrences(sentOccur : List[Occurrences],
+                              curSentence : Sentence,
+                              sentList : List[Sentence]) : List[Sentence] =
+  {
+    if (sentOccur.isEmpty) {
+      return curSentence :: sentList
+    }
+    else {
+      var curSentList : List[Sentence] = Nil
 
+      for (o <- sentOccur) {
+        val wordFromOccur = dictionaryByOccurrences(o)
+        val restSentOccur = sentOccur.filterNot(oc => o == oc)
+
+        for (w <- wordFromOccur) {
+          curSentList = curSentList ::: sentenceFromOccurrences(restSentOccur, w :: curSentence, sentList)
+        }
+      }
+
+      return curSentList ::: sentList
+    }
+  }
+
+  def sentenceRecursion(sentenceOccur: Occurrences,
+                        curSentAnagram : List[Occurrences],
+                        sentAnagrams : List[Sentence]) : List[Sentence] =
+  {
+    if (sentenceOccur.isEmpty) {
+      return sentenceFromOccurrences(curSentAnagram, Nil, Nil) ::: sentAnagrams
+    }
+    else {
+      val sentComb : List[Occurrences] = combinations(sentenceOccur).filter(o => dictionaryByOccurrences.contains(o))
+      var curSentAnagrams : List[Sentence] = Nil
+
+      if (!sentComb.isEmpty) {
+        for (o <- sentComb) {
+          val restSentOccur = subtract(sentenceOccur, o)
+          curSentAnagrams = sentenceRecursion(restSentOccur, o :: curSentAnagram, sentAnagrams) ::: curSentAnagrams
+        }
+      }
+
+      return curSentAnagrams
+    }
+  }
+
+  def sentenceAnagrams(sentence: Sentence): List[Sentence] = {
+    val sentOccurrences : Occurrences = sentenceOccurrences(sentence)
+    return sentenceRecursion(sentOccurrences, Nil, Nil).distinct
+  }
 }
