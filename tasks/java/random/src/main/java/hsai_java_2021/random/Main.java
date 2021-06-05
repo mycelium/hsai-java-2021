21f7a7512b5cdeea341ee1f8cea24c5ce08f7d9f
Mikhail Malyarenko <malyarenko.md@gmail.com>
2021-06-05 14:48:07 +0300

    Java Lab 1 Initial Commit

---
+++ tasks/java/random/src/main/java/hsai_java_2021/random/Main.java
@@ -0,0 +1,13 @@
+package hsai_java_2021.random;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
