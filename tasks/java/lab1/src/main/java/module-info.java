0b8566f26510784f2c08eabfdc8ac056a562998f
Robert Andreev <andreev.robert.a@gmail.com>
2021-06-08 01:59:50 +0300

    Add exports

--- tasks/java/lab1/src/main/java/module-info.java
+++ tasks/java/lab1/src/main/java/module-info.java
@@ -2,4 +2,7 @@ module random.generator {
     requires opencsv;
     requires java.sql;
 
+    exports ru.spbstu.telematics.samples;
+    exports ru.spbstu.telematics.table;
+    exports ru.spbstu.telematics.writers;
 }
