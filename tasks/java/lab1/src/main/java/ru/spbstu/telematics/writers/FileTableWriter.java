2d7a3331f286e8fa8db046e41e62f46ac162797b
Robert Andreev <andreev.robert.a@gmail.com>
2021-06-08 01:00:30 +0300

    Generalize writer

--- tasks/java/lab1/src/main/java/ru/spbstu/telematics/writers/FileTableWriter.java
+++ tasks/java/lab1/src/main/java/ru/spbstu/telematics/writers/FileTableWriter.java
@@ -2,8 +2,24 @@ package ru.spbstu.telematics.writers;
 
 import ru.spbstu.telematics.table.Table;
 
-public abstract class FileTableWriter {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-    public abstract void write(Table table, String filePath);
+abstract class FileTableWriter {
+
+    protected Path createFileWithExtension(String filePath, String extension) throws IOException {
+        Path path = Path.of(filePath);
+        if (!filePath.endsWith(extension)) {
+            throw new IllegalArgumentException("File must have" + extension + "extension");
+        }
+        Files.createDirectories(path.getParent());
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+        return path;
+    }
+
+    abstract void write(Table table, String filePath) throws Exception;
 
 }
