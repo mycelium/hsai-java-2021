0cfeb66dcfc3f372e2b2ce12fadec7e589242df4
Robert Andreev <andreev.robert.a@gmail.com>
2021-06-08 14:05:03 +0300

    Refactor examples and output

--- tasks/java/lab1/src/test/java/ru/spbstu/telematics/AppTest.java
+++ tasks/java/lab1/src/test/java/ru/spbstu/telematics/AppTest.java
@@ -4,24 +4,39 @@ import org.testng.annotations.Test;
 import ru.spbstu.telematics.samples.AutoSamplesFactory;
 import ru.spbstu.telematics.table.Table;
 import ru.spbstu.telematics.writers.CSVTableWriter;
+import ru.spbstu.telematics.writers.DataBaseTableWriter;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class AppTest {
 
-    String path = "src/test/resources/AppTest";
+    final String DIR = "src/test/resources/AppTest";
+    final String CSV_FILE = DIR + "/" + "outCSV.csv";
+    final String DB_FILE = DIR + "/" + "outDB.db";
 
     @Test
-    void SystemTest() throws IOException {
+    void SystemTestCSV() throws IOException {
         Table table = new Table("Table1");
         table
                 .add(AutoSamplesFactory.getNormal("Normal", 1., -10.))
                 .add(AutoSamplesFactory.getPoisson("Poisson", 10))
                 .add(AutoSamplesFactory.getUniform("Uniform", -50., -40.));
-        table.generate(10);
-        CSVTableWriter writer = new CSVTableWriter(path + "/out.csv");
+        table.generate(30);
+        CSVTableWriter writer = new CSVTableWriter(CSV_FILE);
         writer.write(table);
     }
 
+    @Test
+    void SystemTestDB() throws IOException, SQLException {
+        Table table = new Table("Table1");
+        table
+                .add(AutoSamplesFactory.getNormal("Normal", 1., -10.))
+                .add(AutoSamplesFactory.getPoisson("Poisson", 10))
+                .add(AutoSamplesFactory.getUniform("Uniform", -50., -40.));
+        table.generate(30);
+        DataBaseTableWriter writer = new DataBaseTableWriter(DB_FILE);
+        writer.write(table);
+    }
 
 }
