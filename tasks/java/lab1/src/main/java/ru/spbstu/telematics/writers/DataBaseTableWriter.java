0c38ba426b4e6785bc2d48d21b5a96265f4ae2cc
Robert Andreev <andreev.robert.a@gmail.com>
2021-06-08 14:04:34 +0300

    Add drop table and refactor code

--- tasks/java/lab1/src/main/java/ru/spbstu/telematics/writers/DataBaseTableWriter.java
+++ tasks/java/lab1/src/main/java/ru/spbstu/telematics/writers/DataBaseTableWriter.java
@@ -3,7 +3,6 @@ package ru.spbstu.telematics.writers;
 import ru.spbstu.telematics.table.Table;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,40 +11,46 @@ import java.util.List;
 
 public class DataBaseTableWriter extends FileTableWriter {
 
+    public DataBaseTableWriter(String file) throws IOException {
+        super(file);
+        createFileWithExtension(file, ".db");
+    }
+
     @Override
-    public void write(Table table, String filePath) throws IOException, SQLException {
-        Path out = createFileWithExtension(filePath, ".db");
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:" + out.toAbsolutePath().toString());
+    public void write(Table table) throws IOException, SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:" + outPath.toAbsolutePath().toString());
         Statement statement = connection.createStatement();
+        statement.execute("DROP TABLE IF EXISTS " + table.getTableName());
         String creation = generateCreationStatement(table.getTableName(), table.getSamplesName());
         statement.execute(creation);
         String insertion = generateInsertionStatement(table.getTableName(), table.getSamplesName(), table.getTableValues());
         statement.executeUpdate(insertion);
     }
 
-    private String generateCreationStatement(String tableName, String[] columnsName) {
+    private String generateCreationStatement(String tableName, List<String> columnsName) {
         String statement = "CREATE TABLE " + tableName + " (" +
                 "id INTEGER PRIMARY KEY AUTOINCREMENT";
-        for (int i = 0; i < columnsName.length; i++) {
-            statement += ", " + columnsName[i] + "_" + i + " REAL NOT NULL";
+        for (int i = 0; i < columnsName.size(); i++) {
+            statement += ", " + columnsName.get(i) + "_" + i + " REAL NOT NULL";
         }
-        return statement += ");";
+        statement += ");";
+        return statement;
     }
 
-    private String generateInsertionStatement(String tableName, String[] columnsName, List<String[]> tableValues) {
+    private String generateInsertionStatement(String tableName, List<String> columnsName, List<List<Object>> tableValues) {
         StringBuilder statement = new StringBuilder("INSERT INTO " + tableName + " (");
-        for (int i = 0; i < columnsName.length; i++) {
-            statement.append(columnsName[i]).append("_").append(i).append(",");
+        for (int i = 0; i < columnsName.size(); i++) {
+            statement.append(columnsName.get(i)).append("_").append(i).append(",");
         }
         statement.setLength(statement.length() - 1);
         statement.append(") VALUES \n");
-        for (String[] line : tableValues) {
+        for (List<Object> line : tableValues) {
             statement.append("(");
             String prefix = "";
-            for (String elem : line) {
+            for (Object elem : line) {
                 statement.append(prefix);
                 prefix = ",";
-                statement.append(elem);
+                statement.append(elem.toString());
             }
             statement.append("),");
         }
