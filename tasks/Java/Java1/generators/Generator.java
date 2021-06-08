50fb791eb1d4cabbdef9ddbb34e38cc384d665e9
Arctangen <72122996+Arctangen@users.noreply.github.com>
2021-06-09 02:04:33 +0300

    Add files via upload

---
+++ tasks/Java/Java1/generators/Generator.java
@@ -0,0 +1,8 @@
+package com.spbstu.generators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Generator {
+    ArrayList<Double> generate(int n);
+}
---
+++ tasks/Java/Java1/writers/FileTableWriter.java
@@ -0,0 +1,31 @@
+package ru.spbstu.telematics.writers;
+
+import ru.spbstu.telematics.table.Table;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+abstract class FileTableWriter {
+
+    protected Path outPath;
+
+    public FileTableWriter(String out) {
+        this.outPath = Path.of(out);
+    }
+
+    protected Path createFileWithExtension(String filePath, String extension) throws IOException {
+        Path path = Path.of(filePath);
+        if (!filePath.endsWith(extension)) {
+            throw new IllegalArgumentException("File must have" + extension + "extension");
+        }
+        Files.createDirectories(path.getParent());
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+        return path;
+    }
+
+    abstract void write(Table table) throws Exception;
+
+}
