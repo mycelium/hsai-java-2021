f1b1e57d6f5611190b54e52e6e0b0fd50429ca80
Robert Andreev <andreev.robert.a@gmail.com>
2021-06-08 19:42:48 +0300

    Add assertions to AppTest

--- tasks/java/lab1/src/test/java/ru/spbstu/telematics/AppTest.java
+++ tasks/java/lab1/src/test/java/ru/spbstu/telematics/AppTest.java
@@ -1,12 +1,16 @@
 package ru.spbstu.telematics;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.spbstu.telematics.samples.AutoSamplesFactory;
 import ru.spbstu.telematics.table.Table;
 import ru.spbstu.telematics.writers.CSVTableWriter;
 import ru.spbstu.telematics.writers.DataBaseTableWriter;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
 
 public class AppTest {
@@ -14,6 +18,7 @@ public class AppTest {
     final String DIR = "src/test/resources/AppTest";
     final String CSV_FILE = DIR + "/" + "outCSV.csv";
     final String DB_FILE = DIR + "/" + "outDB.db";
+    final Integer GENERATED = 30;
 
     @Test
     public void SystemTestCSV() throws IOException {
@@ -22,9 +27,13 @@ public class AppTest {
                 .add(AutoSamplesFactory.getNormal("Normal 1, -10", 1., -10.))
                 .add(AutoSamplesFactory.getPoisson("Poisson 10", 10))
                 .add(AutoSamplesFactory.getUniform("Uniform [-50; -40]", -50., -40.));
-        table.generate(30);
+        table.generate(GENERATED);
+        Assert.assertNotNull(table.getNumberOfValues());
+        Assert.assertEquals(table.getNumberOfValues(), GENERATED);
         CSVTableWriter writer = new CSVTableWriter(CSV_FILE);
         writer.write(table);
+        Assert.assertTrue(Files.isRegularFile(Path.of(CSV_FILE)));
+        Assert.assertEquals(Files.readAllLines(Path.of(CSV_FILE)).size(), GENERATED + 1);
     }
 
     @Test
@@ -34,9 +43,12 @@ public class AppTest {
                 .add(AutoSamplesFactory.getNormal("Normal", 1., -10.))
                 .add(AutoSamplesFactory.getPoisson("Poisson", 10))
                 .add(AutoSamplesFactory.getUniform("Uniform", -50., -40.));
-        table.generate(30);
+        table.generate(GENERATED);
+        Assert.assertNotNull(table.getNumberOfValues());
+        Assert.assertEquals(table.getNumberOfValues(), GENERATED);
         DataBaseTableWriter writer = new DataBaseTableWriter(DB_FILE);
         writer.write(table);
+        Assert.assertTrue(Files.isRegularFile(Path.of(DB_FILE)));
     }
 
 }
