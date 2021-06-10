7bfbaa7c829f1ce926dfdf403a1ea52ef713dd78
Robert Andreev <andreev.robert.a@gmail.com>
2021-06-10 16:32:50 +0300

    Add logger to json module

--- tasks/java/lab2/json/src/main/java/module-info.java
+++ tasks/java/lab2/json/src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module json {
+    requires log4j.api;
     exports ru.spbstu.telematics.writer.json;
 }
--- tasks/java/lab2/json/src/main/java/ru/spbstu/telematics/writer/json/JSONWriter.java
+++ tasks/java/lab2/json/src/main/java/ru/spbstu/telematics/writer/json/JSONWriter.java
@@ -1,5 +1,8 @@
 package ru.spbstu.telematics.writer.json;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -8,9 +11,12 @@ import java.nio.file.Path;
 
 public class JSONWriter {
 
+    private static Logger logger = LogManager.getLogger(JSONWriter.class);
+
     private Path path;
 
     public JSONWriter(String filePath) throws IOException {
+        logger.info("Create JSONWriter for: " + filePath);
         path = createFileWithExtension(filePath, ".json");
     }
 
@@ -31,11 +37,14 @@ public class JSONWriter {
     private Path createFileWithExtension(String filePath, String extension) throws IOException {
         Path path = Path.of(filePath);
         if (!filePath.endsWith(extension)) {
-            throw new IllegalArgumentException("File must have" + extension + "extension");
+            IllegalArgumentException ex = new IllegalArgumentException("File must have" + extension + "extension");
+            logger.error("File: " + filePath + "has not " + extension, ex);
+            throw ex;
         }
         Files.createDirectories(path.getParent());
         if (!Files.exists(path)) {
             Files.createFile(path);
+            logger.info("Create file: " + filePath);
         }
         return path;
     }
