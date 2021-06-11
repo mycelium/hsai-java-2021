3a92855cfafce2696d75317fd1802eb1f3a05641
EduardLimonov <49101914+EduardLimonov@users.noreply.github.com>
2021-06-11 12:28:32 +0300

    Update Main.scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,6 @@
 package recfun
-import common._
+
+import scala.::
 
 object Main {
   def main(args: Array[String]) {
@@ -9,6 +10,12 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    /*println()
+    println(balance(List('(' , '0', '(')))
+    println(balance(List('(' , '0', ')', ')')))
+
+    println(countChange(200, List(5, 10)))*/
   }
 
   /**
@@ -16,13 +23,33 @@ object Main {
    */
   def pascal(c: Int, r: Int): Int = {
 
+    def factorial(num: Int): Int =
+      if (num <= 1) 1 else num * factorial(num - 1)
+
+    factorial(r) / (factorial(c) * factorial(r - c))
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    var sum: Int = 0
+
+    def found(c: Char): Int =
+      c match {
+        case '(' => 1
+        case ')' => -1
+        case _ => 0
+      }
+
+    def search(ind: Int): Unit = {
+      sum += found(chars.apply(ind))
+      if (ind < chars.length - 1) search(ind + 1)
+    }
+
+    search(0)
+
+    sum == 0
   }
 
   /**
@@ -32,7 +59,15 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
+  def countChange(money: Int, coins: List[Int]): Int =
+    if (money < 0) 0 else if (money == 0) 1 else {
 
-  }
+      var sum: Int = 0
+      for (i <- coins.indices)
+        sum += countChange(money - coins.apply(i), coins)
+
+      sum
+
+    }
+
 }
