d7ab6e8db8ac52e25aa9461f3c63eaef3d307da9
EduardLimonov <49101914+EduardLimonov@users.noreply.github.com>
2021-06-11 14:13:06 +0300

    Update App.java

--- tasks/java/java_lab1/src/main/java/ru/spbstu/hsai/stats/App.java
+++ tasks/java/java_lab1/src/main/java/ru/spbstu/hsai/stats/App.java
@@ -1,13 +1,27 @@
 package ru.spbstu.hsai.stats;
+import ru.spbstu.hsai.outputter.*;
 
-/**
- * Hello world!
- *
- */
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class App 
 {
+    static Dictionary<Param, Double> createParams() {
+        Dictionary<Param, Double> params = new Hashtable<>();
+        params.put(Param.LAMBDA, 4.);
+        params.put(Param.M, 1.);
+        params.put(Param.D, 10.);
+        params.put(Param.MIN, -10.);
+        params.put(Param.MAX, 100.);
+
+        return params;
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Dictionary<Param, Double> params = createParams();
+        Norm.rvs(1000, params, OutputType.CSV, "csvout1.csv");
+        Poisson.rvs(100, params, OutputType.CSV, "csvout2.csv");
+        //Uniform.rvs(10, params, OutputType.database, "dbout.db");
     }
 }
