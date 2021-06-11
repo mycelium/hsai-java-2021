886b45b8168e1053d517b92926a07ab08af46d05
EduardLimonov <49101914+EduardLimonov@users.noreply.github.com>
2021-06-11 12:34:50 +0300

    Update package.scala

--- tasks/scala/3/anagrams/src/main/scala/forcomp/package.scala
+++ tasks/scala/3/anagrams/src/main/scala/forcomp/package.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 package object forcomp {
   val dictionaryPath = List("forcomp", "linuxwords.txt")
 
@@ -19,6 +21,27 @@ package object forcomp {
     } finally {
       wordstream.close()
     }
+  }
+
+  /**
+   * Append two lists recursively.
+   */
+  def append[T](a: List[T], b: List[T]): List[T] = a match {
+    case Nil => b
+    case x :: rest => x :: append(rest, b)
+  }
+
+  /**
+   * Creates an 1d list that consists of all element of given 2d list
+   */
+  def flatten[T](bigList: List[List[T]]): List[T] = {
+    @tailrec
+    def recursion(l: List[List[T]]): List[List[T]] = l match {
+      case Nil => Nil
+      case _ :: Nil => l
+      case x :: y :: rest => recursion(append(x, y) :: rest)
+    }
+    recursion(bigList).last
   }
 
 }
